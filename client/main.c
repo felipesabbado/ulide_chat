9dcecab0b524a6b16440612ad5c13952cc764d20
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-03-22 18:57:49 +0000

    create methods in client

--- client/main.c
+++ client/main.c
@@ -3,17 +3,26 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
+#include <malloc.h>
+
+struct sockaddr_in* createIPv4Address(char *ip, int port);
+
+int createTCPIPv4Socket();
+
+struct sockaddr_in* createIPv4Address(char *ip, int port) {
+
+    struct sockaddr_in * address = malloc(sizeof(struct sockaddr_in));
+    address->sin_family = AF_INET;
+    address->sin_port = htons(port);
+    inet_pton(AF_INET, ip, &address->sin_addr.s_addr);
+    return address;
+}
 
 int main() {
-    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    int socketFD = createTCPIPv4Socket();
+    struct sockaddr_in* address = createIPv4Address("142.250.200.78", 80);
 
-    char* ip = "142.251.214.132";
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(80);
-    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
-
-    int result = connect(socketFD, &address, sizeof(address));
+    int result = connect(socketFD, address, sizeof(*address));
 
     if(result == 0)
         printf("connect was successfull\n");
@@ -28,4 +37,8 @@ int main() {
     printf("Response was: %s\n", buffer);
 
     return 0;
+}
+
+int createTCPIPv4Socket() {
+    return socket(AF_INET, SOCK_STREAM, 0);
 }
