da6f57ff1e60b813e98b6ae166faef6f40c801f8
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-03-25 17:08:23 +0000

    server refactoring working

--- server/util.c
+++ server/util.c
@@ -1,5 +1,9 @@
 #include "util.h"
 
+ACCEPTEDSOCKET* acceptIncomingConnection(int serverSocketFD);
+ACCEPTEDSOCKET acceptedSockets[10]; // 10 is the max number of clients
+int acceptedSocketsCount = 0;
+
 int createTCPIPv4Socket() {
     return socket(AF_INET, SOCK_STREAM, 0);
 }
@@ -15,4 +19,62 @@ struct sockaddr_in* createIPv4Address(char *ip, int port) {
         inet_pton(AF_INET, ip, &address->sin_addr.s_addr);
 
     return address;
+}
+
+void startAcceptingIncomingConnections(int serverSocketFD) {
+    while(1) {
+        ACCEPTEDSOCKET* clientSocket = acceptIncomingConnection(serverSocketFD);
+        acceptedSockets[acceptedSocketsCount++] = *clientSocket;
+
+        receiveAndPrintIncomingDataOnSeparateThread(clientSocket);
+    }
+}
+
+void receiveAndPrintIncomingDataOnSeparateThread(ACCEPTEDSOCKET *pSocket) {
+    pthread_t id;
+    pthread_create(&id, NULL, receiveAndPrintIncomingData, pSocket);
+}
+
+void* receiveAndPrintIncomingData(void* arg) {
+    char buffer[1024];
+    ACCEPTEDSOCKET pSocket = *(ACCEPTEDSOCKET*) arg;
+    int socketFD = pSocket.acceptedSocketFD;
+
+    while(1) {
+        ssize_t amountReceived = recv(socketFD, buffer, 1024, 0);
+
+        if(amountReceived > 0) {
+            buffer[amountReceived] = 0;
+            printf("%s\n", buffer);
+
+            sendReceivedMessageToTheOtherClients(buffer, socketFD);
+        }
+
+        if(amountReceived == 0)
+            break;
+    }
+
+    close(socketFD);
+}
+
+void sendReceivedMessageToTheOtherClients(char *buffer, int socketFD) {
+    for(int i = 0; i < acceptedSocketsCount; ++i)
+        if(acceptedSockets[i].acceptedSocketFD != socketFD)
+            send(acceptedSockets[i].acceptedSocketFD, buffer, strlen(buffer), 0);
+}
+
+ACCEPTEDSOCKET* acceptIncomingConnection(int serverSocketFD) {
+    struct sockaddr_in clientAddress;
+    int clientAddressSize = sizeof(struct sockaddr_in);
+    int clientSocketFD = accept(serverSocketFD, &clientAddress, &clientAddressSize);
+
+    ACCEPTEDSOCKET* acceptedSocket = malloc(sizeof(ACCEPTEDSOCKET));
+    acceptedSocket->address = clientAddress;
+    acceptedSocket->acceptedSocketFD = clientSocketFD;
+    acceptedSocket->acceptedSuccessfully = clientSocketFD > 0;
+
+    if(!acceptedSocket->acceptedSuccessfully)
+        acceptedSocket->error = clientSocketFD;
+
+    return acceptedSocket;
 }
--- server/util.h
+++ server/util.h
@@ -7,8 +7,28 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include <malloc.h>
+#include <unistd.h>
+#include <pthread.h>
+
+struct AcceptedSocket {
+    int acceptedSocketFD;
+    struct sockaddr_in address;
+    int error;
+    int acceptedSuccessfully; // boolean
+};
+
+typedef struct AcceptedSocket ACCEPTEDSOCKET;
 
 int createTCPIPv4Socket();
+
 struct sockaddr_in* createIPv4Address(char *ip, int port);
+
+void* receiveAndPrintIncomingData(void *arg);
+
+void startAcceptingIncomingConnections(int serverSocketFD);
+
+void receiveAndPrintIncomingDataOnSeparateThread(ACCEPTEDSOCKET *pSocket);
+
+void sendReceivedMessageToTheOtherClients(char *buffer, int socketFD);
 
 #endif //UTIL_UTIL_H
