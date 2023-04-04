ddeef4920180ecb2962787ebbe6944b0c599cdcf
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-04-04 23:41:39 +0100

    public room alpha test

--- client/main.c
+++ client/main.c
@@ -2,8 +2,8 @@
 
 int main(int argc, char *argv[]) {
     clientSocket_t clientSocket;
-    createSocketConnection("127.0.0.1", &clientSocket);
-    //createSocketConnection(argv[1], *clientSocket);
+    //createSocketConnection("127.0.0.1", &clientSocket);
+    createSocketConnection(argv[1], &clientSocket);
     int socketFD = clientSocket.clientSocketFD;
 
     startListeningAndPrintMessagesOnNewThread(&clientSocket);
