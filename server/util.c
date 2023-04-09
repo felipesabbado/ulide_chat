e65e68370b8738c2e0f97e9798b10da10dd8448a
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-04-09 18:04:23 +0100

    some minor refactoring in server

--- server/util.c
+++ server/util.c
@@ -7,32 +7,40 @@ int acceptedSocketsCount = 0;
 room_t rooms[MAX_ROOMS];
 pthread_mutex_t room_mutex[MAX_ROOMS];
 
-int createSocketConnection() {
-    int serverSocketFD = socket(AF_INET, SOCK_STREAM, 0);
-    struct sockaddr_in *serverAddress = createIPv4Address("",PORT);
+int createSocketConnection(char *ip, int port) {
+    struct sockaddr_in serverAddress;
 
-    int result = bind(serverSocketFD,serverAddress,sizeof(*serverAddress));
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+
+    if(strlen(ip) == 0)
+        serverAddress.sin_addr.s_addr = INADDR_ANY;
+    else
+        inet_pton(AF_INET, ip, &serverAddress.sin_addr.s_addr);
+
+    int serverSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serverSocketFD < 0) {
+        perror("Socket creation error");
+        close(serverSocketFD);
+        exit(EXIT_FAILURE);
+    }
+
+    int result = bind(serverSocketFD,
+                      (const struct sockaddr *) &serverAddress,
+                              sizeof(serverAddress));
+
     if(result == 0)
         printf("Socket was bound successufuly\n");
-    else
-        exit(1);
+    else {
+        perror("Socket binding error");
+        close(serverSocketFD);
+        exit(EXIT_FAILURE);
+    }
 
     listen(serverSocketFD, MAX_CLIENTS);
 
     return serverSocketFD;
-}
-
-struct sockaddr_in *createIPv4Address(char *ip, int port) {
-    struct sockaddr_in *address = malloc(sizeof(struct sockaddr_in));
-    address->sin_family = AF_INET;
-    address->sin_port = htons(port);
-
-    if(strlen(ip) == 0)
-        address->sin_addr.s_addr = INADDR_ANY;
-    else
-        inet_pton(AF_INET, ip, &address->sin_addr.s_addr);
-
-    return address;
 }
 
 void startAcceptingIncomingConnections(int serverSocketFD) {
