5c4f9ada820a12b5d3acd58c86f706ec0fb8ba96
Felipe <90115621+felipesabbado@users.noreply.github.com>
2023-04-25 17:50:20 +0100

    update host

--- client/main.c
+++ client/main.c
@@ -1,12 +1,12 @@
 #include "util.h"
 
 int main(int argc, char *argv[]) {
-    /*if(argc != 2) {
+    if(argc != 2) {
         printf("Usage: ./client <ip>\n");
         exit(EXIT_FAILURE);
-    }*/
+    }
 
-    int socketFD = createSocketConnection("127.0.0.1", PORT);
+    int socketFD = createSocketConnection(argv[1], PORT);
 
     startListeningAndPrintMessagesOnNewThread(&socketFD);
     sendMessagesToServer(socketFD);
